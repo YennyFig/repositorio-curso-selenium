6fd577d933d848386acb75d8a7e9a8cf9fc40256
PC <PC@10.242.3.19>
2022-02-08 19:37:19 -0500

    Update creacion clases

---
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio1.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio4.java
@@ -0,0 +1,25 @@
+package com.java;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// Declara 2 variables numéricas enteras (con el valor que desees), he indica cual es mayor de los dos. 
+		// Si son iguales indicarlo también. Ves cambiando los valores para comprobar que funciona
+
+		
+		int a = 7;
+		int b = 20;
+		
+		if (a > b) {
+			System.out.println(a + " Es numero mayor " + b);
+		}
+		
+		if (a < b) {
+			System.out.println(a + " Es numero menor " + b);
+	    }
+		
+		if (a == b) {
+			System.out.println(a + " Es numero igual " + b);
+	    }
+	}
+}
