cb401473bb0cbae8e617491afb103bddeee14955
PC <PC@10.242.3.19>
2022-02-11 19:36:21 -0500

    Add Ejercicio3

---
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio3.java
@@ -0,0 +1,48 @@
+package com.java;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+
+		/*Escribe la declaración más apropiada para cada una de las siguientes variables. El nombre de cada una indica el tipo de dato que contendrá y servirá para determinar el 
+		 * tipo de dato más adecuado en la declaración de la variable.
+
+		a) edad                                                b) códigoPostal
+		c) altura                                              d) genero (valores: H: hombre, M: mujer)                   
+		e) nombre                                              f) númeroDeHijos
+		g) iva (valores: % a aplicar)                          h) tallaCamisa (valores numéricos)
+		i) peso                                                j) precio 
+		k) alumnoRepetidor  (valores: SI/NO)                   l) mensaje
+		m) letra                                               n) mayorEdad                                               
+		o) minutos                                             p) días
+		q) matriculaCoche                                      r) contador
+		s) mayorDeEdad  (valores: verdadero/falso)             t) tallaCamiseta (valores: S, L, XL...)
+
+
+		int edad;
+		int codpostal;
+		double altura;
+		char genero;
+		String nombre;
+		int numhijos;
+		double iva;
+		int talla;
+		double peso;
+		double precio;
+		String alumnoRepetidor;
+		String mensaje;
+		char letra;
+		int minutos;
+		String dias;
+		String matricula;
+		int contador;
+		boolean mayorDeEdad;
+		String tallacamiseta;
+		 * 
+		 */
+		
+		
+
+	}
+
+}
