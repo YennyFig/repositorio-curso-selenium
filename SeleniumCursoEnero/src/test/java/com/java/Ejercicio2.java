bd8bc166703934e756343d6985f294e1a676b86a
PC <PC@10.242.3.19>
2022-02-11 19:33:08 -0500

    Add Ejercicio2

---
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio2.java
@@ -0,0 +1,31 @@
+package com.java;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+
+		/*Crear una clase (public static void main) con 4 variables tipo String con los siguientes valores:
+
+		Var 1 = “Cool”
+		Var 2 = “Selenium con Java”
+		Var 3 = “Curso de”
+		Var 4 = “Testers”
+
+		El programa debe imprimir el siguiente mensaje utilizando la concatenación de Java:
+
+		“Curso de Selenium con Java CoolTesters”
+		 * 
+		 */
+		
+		String Var1 = "Cool";
+		String Var2 = "Selenium con Java";
+		String Var3 = "Curso de";
+		String Var4 = "Testers";
+		
+		System.out.print(Var3  + " " + Var2 + " " + Var1 + Var4);
+		
+		
+	}
+
+}
