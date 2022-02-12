a3b075073bf01230a53be96d7321abd56d612caf
PC <PC@10.242.3.19>
2022-02-11 19:38:11 -0500

    Add Ejercicio1

--- SeleniumCursoEnero/src/test/java/com/java/Ejercicio1.java
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio1.java
@@ -4,8 +4,23 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		
+		// Crear una clase (public static void main) con tres variables de tipo double e imprimir la suma, resta, multiplicación y división de las 3 variables.
 		
-
+		double a = 1.5;
+		double b = 9.8;
+		double c = 4.7;
+		
+		double suma = a+b+c;
+		double resta = a-b-c;
+		double mult = a*b*c;
+		double div = a/b/c;
+		
+		System.out.println("Resultado de la suma " + suma);
+		System.out.println("Resultado de la resta " + resta);
+		System.out.println("Resultado de la multiplicación " + mult);
+		System.out.println("Resultado de la división " + div);
+		
+	
 	}
 
 }
