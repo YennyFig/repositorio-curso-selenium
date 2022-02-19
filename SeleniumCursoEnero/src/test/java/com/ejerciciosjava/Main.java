60fb94d2eb678fda183058d6c56a506e0e013775
PC <PC@10.242.3.19>
2022-02-18 19:51:40 -0500

    Commit, se crea la clase Main

---
+++ SeleniumCursoEnero/src/test/java/com/ejerciciosjava/Main.java
@@ -0,0 +1,14 @@
+package com.ejerciciosjava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(Cat.isCatPlaying(false, 15)? "El gato esta jugando":"El gato no esta jugando" );
+		
+		
+
+	}
+
+}
